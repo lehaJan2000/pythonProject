82ddb7ee5c843f9dfe81927d394efc30506e5ea8
gr423_soaro <soldatov.alex2000@mail.ru>
2023-10-10 12:59:00 +0700

    10.10.23

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 
-# Create your views here.
+
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    # Доступные книги (статус = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()  # Метод 'all()' применён по умолчанию.
+
+    # Отрисовка HTML-шаблона index.html с данными внутри
+    # переменной контекста context
+    return render(
+        request,
+        'index.html',
+        context={'num_books': num_books, 'num_instances': num_instances,
+                 'num_instances_available': num_instances_available, 'num_authors': num_authors},
+    )
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 2
+
+
+def get_queryset(self):
+    return Book.objects.filter(title__icontains='war')[:5]  # Получить 5 книг, содержащих 'war' в заголовке
+
+
+def get_context_data(self, **kwargs):
+    # В первую очередь получаем базовую реализацию контекста
+    context = super(BookListView, self).get_context_data(**kwargs)
+    # Добавляем новую переменную к контексту и инициализируем её некоторым значением
+    context['some_data'] = 'This is just some data'
+    return (context)
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
